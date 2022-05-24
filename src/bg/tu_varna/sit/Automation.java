3bb0a1f2fe51bd823ceaebb54ea4e726410ba03e
tedonikolov <teonikolov01@abv.bg>
2022-05-25 02:31:41 +0300

    Added comments for JavaDoc

--- src/bg/tu_varna/sit/Automation.java
+++ src/bg/tu_varna/sit/Automation.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The Automation class represent finite state machine.
+ * The Automation class represent finite state machine
  * @author Teodor
  * @version 1.0
  * @see Symbol
